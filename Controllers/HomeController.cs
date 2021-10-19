f317c75aa75e9c07ac45237837358a4db92ebb98
mohammad mustafa <mohammad.utdcompany@gmail.com>
2021-10-20 02:13:06 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var prods = await _db.Products.ToListAsync();
+            return View(prods);
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            var rand = new Random();
+            var p = new Product
+            {
+                Name = "Works" + rand.Next(1, 100)
+            };
+
+            await _db.Products.AddAsync(p);
+            
+            await _db.SaveChangesAsync();
+            return Redirect("/");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> op): base(op)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
